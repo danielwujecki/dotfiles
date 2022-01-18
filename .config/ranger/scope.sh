cabef5086610e93b2da95c9de16a4ce61fca7d08
danielgolf <daniel.golf@web.de>
2022-01-18 13:27:01 +0100

    grub and ranger config

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -52,16 +52,16 @@ handle_extension() {
         ## Archive
         a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
         rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
-            atool --list -- "${FILE_PATH}" && exit 5
-            bsdtar --list --file "${FILE_PATH}" && exit 5
+            #atool --list -- "${FILE_PATH}" && exit 5
+            #bsdtar --list --file "${FILE_PATH}" && exit 5
             exit 1;;
         rar)
             ## Avoid password prompt by providing empty password
-            unrar lt -p- -- "${FILE_PATH}" && exit 5
+            #unrar lt -p- -- "${FILE_PATH}" && exit 5
             exit 1;;
         7z)
             ## Avoid password prompt by providing empty password
-            7z l -p -- "${FILE_PATH}" && exit 5
+            #7z l -p -- "${FILE_PATH}" && exit 5
             exit 1;;
 
         ## PDF
